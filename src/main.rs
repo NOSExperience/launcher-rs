59d2c20c46189cc27cde44f25bcf2ae441259b52
twetzel59 <twetzel59@gmail.com>
2017-10-07 12:17:32 -0400

    Begin setting up UI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate gio;
+extern crate gtk;
+
+use gio::{ApplicationExt, ApplicationFlags};
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Inhibit, WindowPosition};
+
+const TITLE: &str = "NOSE Launcher";
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::new(None, ApplicationFlags::empty()).unwrap();
+    //app.set_menubar();
+
+    let win = ApplicationWindow::new(&app);
+    win.set_title(TITLE);
+    win.set_show_menubar(true);
+    win.set_default_size(800, 500);
+    win.set_position(WindowPosition::Center);
+    win.show_all();
+
+    /*
+    win.connect_delete_event(|_, _| {
+        gtk::main_quit();
+
+        Inhibit(false)
+    });
+
+    gtk::main();
+    */
 }
